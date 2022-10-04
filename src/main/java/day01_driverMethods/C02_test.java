42a7600c603779bffe1ffdc4a05723421f5a3f98
aymurat111 <aymurat111@gmail.com>
2022-10-04 09:47:16 +0200

    test

--- src/main/java/day01_driverMethods/C02_test.java
+++ src/main/java/day01_driverMethods/C02_test.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class C02_test {
         public static void main(String[] args) {
-            System.setProperty("webdriver.chrome.driver","src/resources/chromedriver");
+            System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
             WebDriver driver = new ChromeDriver();
             driver.get("https://www.amazon.com");
 }}
