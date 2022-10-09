c5cfc99a576d2abeb03524b1aad13394c48aa220
aymurat111 <aymurat111@gmail.com>
2022-10-09 21:13:05 +0200

    nutella

---
+++ src/test/java/Practice/testBUG.java
@@ -0,0 +1,25 @@
+package Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class testBUG {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver","src/resources/chromedriver.exe");
+        WebDriver driver= new ChromeDriver();
+
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait( Duration.ofSeconds(5));
+
+        driver.get("https://www.hepsiburada.com");
+
+        WebElement aramaMotoru= driver.findElement(By.className( "desktopOldAutosuggestTheme-UyU36RyhCTcuRs_sXL9b"));
+        aramaMotoru.sendKeys( "elektronik" + Keys.ENTER );
+
+    }
+}
--- src/test/java/day03_webElements_locators/C01_findElement_Nutella.java
+++ src/test/java/day03_webElements_locators/C01_findElement_Nutella.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
 
-public class C01_findElement {
+public class C01_findElement_Nutella {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver","src/test/chromedriver.exe");
         WebDriver driver= new ChromeDriver();
