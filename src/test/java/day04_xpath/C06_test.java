7a01cd7baa1a95c4bac5cff12a04d66ff4d7c90a
aymurat111 <aymurat111@gmail.com>
2022-10-15 20:07:07 +0200

    test

---
+++ src/test/java/day04_xpath/C06_test.java
@@ -0,0 +1,31 @@
+package day04_xpath;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.time.Duration;
+
+public class C06_test {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty( "webdriver.chrome.driver","src/test/chromedriver.exe" );
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait( Duration.ofSeconds( 12 ) );
+        driver.get( "https://www.amazon.de" );
+        Thread.sleep( 1000 );
+        WebElement amazonAramaButonu= driver.findElement( By.id("twotabsearchtextbox"));
+        amazonAramaButonu.click();
+        amazonAramaButonu.sendKeys( "samsung a52" );
+        amazonAramaButonu.submit();
+        amazonAramaButonu.clear();
+        amazonAramaButonu.sendKeys( "samsung a72" );
+       amazonAramaButonu.submit();
+
+        driver.close();
+
+
+
+
+    }
+}
