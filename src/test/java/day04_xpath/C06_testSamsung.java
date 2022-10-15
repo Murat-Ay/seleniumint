da77d46bab0c7856eb508a33c16bdb073c456983
aymurat111 <aymurat111@gmail.com>
2022-10-15 20:10:13 +0200

    ttest

--- src/test/java/day04_xpath/C06_testSamsung.java
+++ src/test/java/day04_xpath/C06_testSamsung.java
@@ -1,12 +1,11 @@
 package day04_xpath;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
-public class C06_test {
+public class C06_testSamsung {
     public static void main(String[] args) throws InterruptedException {
         System.setProperty( "webdriver.chrome.driver","src/test/chromedriver.exe" );
         WebDriver driver=new ChromeDriver();
