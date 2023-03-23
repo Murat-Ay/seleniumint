5d7c494bb2582843d8b348b98b0e1ae249aace19
aymurat111 <aymurat111@gmail.com>
2023-03-23 16:07:17 +0100

    sss

--- src/test/java/Practice/testBUG.java
+++ src/test/java/Practice/testBUG.java
@@ -15,6 +15,8 @@ public class testBUG {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait( Duration.ofSeconds(5));
 
+
+
         driver.get("https://www.hepsiburada.com");
 
         WebElement aramaMotoru= driver.findElement(By.className( "desktopOldAutosuggestTheme-UyU36RyhCTcuRs_sXL9b"));
